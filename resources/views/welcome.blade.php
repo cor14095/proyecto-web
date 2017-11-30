ed74024648f299c1d823354656c8a7201050b47b
cor14095 <ale.crts1996@gmail.com>
2017-11-30 16:59:57 -0600

    Done the first part, missing menú part.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,71 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <title>Reed - Design & Code</title>
 
-        <title>Laravel</title>
+    <!-- CSS  -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  </head>
+  <body>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <div class="main">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+      <div class="logo">
+      </div>
 
-            .full-height {
-                height: 100vh;
-            }
+      <button class="menu-btn" aria-label="Contact">
+				<span class="menu-btn__text">Contact</span>
+				<span class="menu-btn__icon">
+					<span></span>
+					<span></span>
+					<span></span>
+				</span>
+			</button>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+      <div class="menu">
+        <h1>Ginventory</h1>
+        <ul class="menu-list">
+          <li class="active"><a href="#">Ginventory</a></li>
+          <li><a href="#">VR Sessions</a></li>
+          <li><a href="#">Le Voyagist</a></li>
+          <li><a href="#">Riftworld</a></li>
+        </ul>
+      </div>
 
-            .position-ref {
-                position: relative;
-            }
+      <ul class="social-list">
+        <li class="social-list__item">
+          <svg class="icon icon--tw" viewBox="0 0 16 16" aria-hidden="true">
+            <path d="M16,3c-0.6,0.3-1.2,0.4-1.9,0.5c0.7-0.4,1.2-1,1.4-1.8c-0.6,0.4-1.3,0.6-2.1,0.8c-0.6-0.6-1.5-1-2.4-1C9.3,1.5,7.8,3,7.8,4.8c0,0.3,0,0.5,0.1,0.7C5.2,5.4,2.7,4.1,1.1,2.1c-0.3,0.5-0.4,1-0.4,1.7c0,1.1,0.6,2.1,1.5,2.7c-0.5,0-1-0.2-1.5-0.4c0,0,0,0,0,0c0,1.6,1.1,2.9,2.6,3.2C3,9.4,2.7,9.4,2.4,9.4c-0.2,0-0.4,0-0.6-0.1c0.4,1.3,1.6,2.3,3.1,2.3c-1.1,0.9-2.5,1.4-4.1,1.4c-0.3,0-0.5,0-0.8,0c1.5,0.9,3.2,1.5,5,1.5c6,0,9.3-5,9.3-9.3c0-0.1,0-0.3,0-0.4C15,4.3,15.6,3.7,16,3z"></path>
+          </svg>
+					<a href="https://twitter.com/ree33d" target="_blank">Twitter</a>
+				</li>
+				<li class="social-list__item">
+				  <svg class="icon icon--fb" viewBox="0 0 16 16" aria-hidden="true">
+            <path d="M14,0H2C0.9,0,0,0.9,0,2v12c0,1.1,0.9,2,2,2h6V9.6H6.6V7.3H8c0,0,0-1.5,0-1.9c0-0.3-0.1-2.6,2.9-2.6h2.1v2.4h-1.5c-0.3,0-0.6,0.3-0.6,0.5c0,0.2,0,1.5,0,1.5s1.9,0,2.1,0c-0.1,1.2-0.3,2.3-0.3,2.3h-1.9V16H14c1.1,0,2-0.9,2-2V2C16,0.9,15.1,0,14,0z"></path>
+          </svg>
+          <a href="https://www.facebook.com/reedbelgium/" target="_blank">Facebook</a>
+				</li>
+			</ul>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+      <div class="horizontal-bars">
+        <div class="bar-1">
         </div>
-    </body>
+        <div class="bar-2">
+        </div>
+        <div class="bar-3">
+        </div>
+      </div>
+
+    </div>
+
+    <!--  Scripts-->
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="js/materialize.js"></script>
+    <script src="js/init.js"></script>
+
+  </body>
 </html>
